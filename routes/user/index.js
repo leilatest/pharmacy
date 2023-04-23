3a4c7e89583dab938e0c6144a53a75ba5454dc2d
leila ben chaalia <119710623+leilatest@users.noreply.github.com>
2023-04-23 19:35:55 +0100

    pharmacy

---
+++ routes/user/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const user = require("../../models/user");
+const pharmacy = require("../../models/pharmacy");
+
+router.post("/register", require("./register"));
+router.post("/login", require("./login"));
+
+router.post("/addpharmacy", require("./addpharmacy"));
+router.get("/addresspharmacy/:address", require("./addresspharmacy"));
+router.get("/listpharmacy", require("./listpharmacy"));
+
+module.exports = router;
